87938771c7525cad38338bba25be7799b1a162e5
peter-resnick <32655722+peter-resnick@users.noreply.github.com>
2018-02-19 17:08:59 -0600

    PyBank
    incorrect version of code was originally uploaded. This is an updated version

--- PyBank.py
+++ PyBank.py
@@ -1,22 +1,47 @@
 import os 
 import csv
 
-csvpath = os.path.join("..","NUCHI201801DATA4-Class-Repository-Data","MWS","Homework","03-Python","Instructions","PyBank","budget_data_1.csv")
+csvpath = os.path.join("..","..","PyBank","raw_data","budget_data_1.csv")
+k = 0
+with open(csvpath, newline="") as csvfile:
+    dataSet = csv.reader(csvfile, delimiter=",")
+
+   num_months = 0
+    for row in dataSet:
+        num_months = num_months + 1
+    print("Total Months: " + str(num_months - 1))
 
 with open(csvpath, newline="") as csvfile:
-    dataset = csv.reader(csvfile, delimiter=",")
+    dataSet = csv.reader(csvfile, delimiter=",")
+    for rows in dataSet:
+        k = k + int(row[1])
 
-num_months = sum(1 for row in dataset)
-print("Total Months: " + str(num_months))
+   print("Total Revenue: $" + str(k))
 
-for rows in dataset:
-    k = k + row[1]
+   avg_change = k/num_months
+    print("Average Revenue Change: $" + str(round(avg_change,2)))
 
-print("Total Revenue: $" + str(k))
+max_rev = 0
+max_date = 0
 
-avg_change = k/num_months
-print("Average Revenue Change: $" + str(avg_change))
+with open(csvpath, newline="") as csvfile:
+    dataSet = csv.reader(csvfile, delimiter=",")
+    next(dataSet)
+    
+   for rows in dataSet:
+        if int(row[1]) > int(max_rev):
+            max_rev = int(row[1])
+            max_date = row[0]
 
-#for rows in csvreader:
- #  'if row[0] == row.next()[0]
-  #      k = k + 1
+   print("Greatest Increase in Revenue: " + str(max_date) + " ($" + str(max_rev) + ")")
+
+with open(csvpath, newline="") as csvfile:
+    dataSet = csv.reader(csvfile, delimiter=",")
+    min_rev = 10000000000
+    min_date = 0
+    for rows in dataSet:
+        if int(row[1]) < int(min_rev):
+            min_rev = row[1]
+            min_date = row[0]
+
+   print("Greatest Decrease in Revenue: " + str(min_date) + " ($" + str(min_rev) + ")")
